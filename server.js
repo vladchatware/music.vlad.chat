5c5ebfaa44b1fae0e30bb52aa299fa45b1e9eaa6
Vlad Rimsha <vlad@jelly.ninja>
2025-03-04 12:01:30 +0700

    Prepare the project

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+
+app.use((_, res, next) => {
+    res.set({
+        "Cross-Origin-Opener-Policy": "same-origin",
+        "Cross-Origin-Embedder-Policy": "require-corp",
+        "Cross-Origin-Resource-Policy": "cross-origin",
+        "Origin-Agent-Cluster": "?1",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Range"
+    })
+    next()
+})
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000')
+})
