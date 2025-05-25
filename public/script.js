6decbaf77963549a51246aa5a4f7c09556f57f27
Vlad Rimsha <vlad@jelly.ninja>
2025-05-25 14:26:14 +0700

    Bypass FileProxtCache and load directly from assets

--- public/script.js
+++ public/script.js
@@ -3,24 +3,15 @@ import {FilesetResolver, LlmInference} from 'https://cdn.jsdelivr.net/npm/@media
 import FileProxyCache from 'https://cdn.jsdelivr.net/gh/jasonmayes/web-ai-model-proxy-cache@main/FileProxyCache.min.js';
 
 const modelFileNameRemote = 'https://storage.googleapis.com/jmstore/WebAIDemos/models/Gemma2/gemma2-2b-it-gpu-int8.bin';
-const modelFileName = 'https://100.85.190.119:3000/gemma2-2b-it-gpu-int8.bin';
+const modelFileName = 'https://localhost:3000/gemma2-2b-it-gpu-int8.bin';
 
 const main = async () => {
-  // Attempt to load from cache or localhost.
-  let dataUrl = await FileProxyCache.loadFromURL(modelFileName, (text) => console.log('loading model:', text));
-  // If failed due to no local file stored, fetch cloud version instead from cache or remote.
-  if (dataUrl === null) {
-   dataUrl = await FileProxyCache.loadFromURL(modelFileNameRemote, (text) => console.log('loading remote model:', text));
-  }
-
-  console.log('Model downloaded. Initializing on GPU... Please wait.')
-
   const genaiFileset = await FilesetResolver.forGenAiTasks(
       'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-genai/wasm');
 
   const llm = await LlmInference.createFromOptions(genaiFileset, {
     baseOptions: {
-      modelAssetPath: dataUrl
+      modelAssetPath: 'gemma2-2b-it-gpu-int8.bin'
     },
     maxTokens: 8000,
     topK: 1,
@@ -28,7 +19,8 @@ const main = async () => {
     randomSeed: 64
   });
 
-  console.log('llm', llm)
+  const res = await llm.generateResponse('Hello, how are you?')
+  console.log(res)
 }
 
 main()
