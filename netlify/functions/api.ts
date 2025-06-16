9b3df5210f902be62ebab8cd99e4b251751ab78c
Vlad Rimsha <vlad@jelly.ninja>
2025-06-16 17:02:05 +0700

    Add support for Netlify functions

---
+++ netlify/functions/api.ts
@@ -0,0 +1,12 @@
+import express, { Router } from 'express'
+import serverless from 'serverless-http'
+
+const app = express()
+
+const router = Router()
+
+router.get('/hello', (req, res) => res.send('Hello World'))
+
+app.use('/api/', router)
+
+export const handler = serverless(app)
