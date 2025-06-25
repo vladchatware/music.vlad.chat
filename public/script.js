34af6a1900e7dfb1c51c33d4bf610b49ee21928b
Vlad Rimsha <vlad@jelly.ninja>
2025-06-25 18:29:15 +0700

    Refine the player

--- public/script.js
+++ public/script.js
@@ -1,60 +1,105 @@
-import { ask, messages, speech } from './ai.js'
+import { ask, messages, speech as play_sound, speech } from './ai.js'
 
 const widgetIframe = document.getElementById('sc-widget')
-const widget = SC.Widget(widgetIframe)
+window.player = SC.Widget(widgetIframe)
 
 const revibe_button = document.getElementById('revibe')
 const remix_indicator = document.getElementById('remix-indicator')
+const inner_container = document.getElementById('inner-container')
 
-widget.bind(SC.Widget.Events.READY, async () => {
+window.player.bind(SC.Widget.Events.READY, async () => {
+  inner_container.style.visibility = 'visible'
   revibe_button.addEventListener('click', async () => {
     await start_sequence()
   })
-  widget.bind(SC.Widget.Events.FINISH, async () => {
+  window.player.bind(SC.Widget.Events.FINISH, async () => {
     // Hold on, lemme vibe it out
+    inner_container.style.visibility = 'visible'
     await revibe()
   });
-  widget.bind(SC.Widget.Events.ERROR, async () => {
+  window.player.bind(SC.Widget.Events.PAUSE, async () => {
+    inner_container.style.visibility = 'visible'
+  })
+  window.player.bind(SC.Widget.Events.ERROR, async () => {
     // Error encountered
+    inner_container.style.visibility = 'visible'
     console.log('Song error')
   })
 })
 
-const start_sequence = async () => {
-  remix_indicator.style.visibility = 'visible'
-  remix_indicator.innerText = 'Hello, I am a virtual DJ, let me play some music.'
-  await speech(remix_indicator.innerText)
-  revibe_button.disabled = true
-  await revibe()
-  revibe_button.disabled = false
+window.play_sound = async (text) => {
+  window.player.setVolume(30)
+  const blob = await speech(text)
+  return new Promise(async (res, rej) => {
+    const audio = document.createElement('audio')
+    audio.src = await URL.createObjectURL(blob)
+    audio.controls = true
+    audio.style.display = 'none'
+    document.body.appendChild(audio)
+    audio.play()
+    audio.addEventListener('ended', () => {
+      audio.remove()
+      window.player.setVolume(100)
+      res()
+    })
+  })
 }
 
-const play_artist = async (url) => {
+window.start_sequence = async () => {
+  if (revibe_button.disabled) return
+  if (queue.length) return revibe()
+  revibe_button.disabled = true
+  remix_indicator.innerHTML = '<p>Hello, I am a virtual DJ, let me play some music.</p>'
+  await window.play_sound('Hello, I am a virtual DJ, let me play some music.')
+  return revibe()
+}
+
+window.play_artist = async (url) => {
   await new Promise((callback) => {
-    widget.load(url, { callback })
+    window.player.load(url, {
+      auto_play: true,
+      show_artwork: true,
+      show_comments: true,
+      show_playcount: true,
+      show_user: true,
+      height: 1280,
+      callback
+    })
   })
 
-  return widget.play()
+  return window.player.play()
 }
 
-let history = []
+const history = {
+  get: () => window.localStorage.getItem('history'),
+  push: (name) => {
+    const _history = history.get()
+    if (!_history) window.localStorage.setItem('history', `${name}`)
+
+    window.localStorage.setItem('history', `${name}, ${history.get()}`)
+  }
+}
+
 let queue = []
 
 const revibe = async () => {
   if (queue.length) {
+    inner_container.style.visibility = 'visible'
     const track = queue.shift()
     history.push(track.track)
-    remix_indicator.innerText = track.justification
-    await speech(remix_indicator.innerText)
-    return play_artist(track.track)
+    remix_indicator.innerHTML = `<p>${track.justification}</p>`
+    await window.play_sound(track.justification)
+    inner_container.style.visibility = 'hidden'
+    return window.play_artist(track.track)
   }
 
-  remix_indicator.style.visibility = 'visible'
-  remix_indicator.innerText = 'Hold on, lemme vibe it out!'
-  await speech(remix_indicator.innerText)
-  const res = await ask(`
-Find some songs from soundcloud for Frutiger Aero aethtetics.
-Previous tracks: ${history.join(', ')}
+  inner_container.style.visibility = 'visible'
+  remix_indicator.innerHTML = '<p>Hold on, lemme vibe it out!</p>'
+  const [res] = await Promise.all([
+    ask(`
+Find some songs from soundcloud for Star Wars Flashback Disco
+like as if it was in older star wars, dont repeat previous tracks
+Previous tracks: ${history.get()}
 Example justifications:
 - Up next is "Artist Name" because...
 - Moving to our next pick is "Artist Name" because...
@@ -66,10 +111,13 @@ Respond with json object:
       "track": "https://api.soundcloud.com/tracks/13692671"
   }]
 }
-and nothing else.`)
+and nothing else.`),
+    window.play_sound('Hold on, lemme vibe it out!')
+  ])
   const payload = JSON.parse(res.output[2].content[0].text)
-  console.log(payload)
+  payload.tracks.map(track => console.log(`${track.justification}`))
   queue = payload.tracks
 
+  revibe_button.disabled = false
   return revibe()
 }
