87766c6a592239c7fea4c9db89f23204063cdca3
Vlad Rimsha <vlad@jelly.ninja>
2025-06-26 16:14:42 +0700

    CRITICAL Netlify proxy timeout cannot be increased

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -4,6 +4,8 @@ import { createProxyMiddleware } from "http-proxy-middleware"
 
 const app = express()
 
+app.use(express.json({limit: '100mb'}))
+
 app.use('/api', createProxyMiddleware({
   target: 'https://api.openai.com/v1',
   changeOrigin: true,
