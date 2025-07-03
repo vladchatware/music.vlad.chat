c55623fef3de5d73f248ff104afd13e6bf8eb550
Vlad Rimsha <vlad@jelly.ninja>
2025-07-03 17:49:43 +0700

    Cleanup

--- server.ts
+++ server.ts
@@ -1,9 +1,10 @@
-import express, { Router } from 'express'
-import serverless from 'serverless-http'
+import express from "express"
+import fs from 'node:fs'
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js"
-import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js"
+import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
+import { isInitializeRequest } from "@modelcontextprotocol/sdk/types.js"
 import { z } from "zod"
-import { getAccessToken, tracks, users, playlists } from '../../soundcloud'
+import { getAccessToken, tracks, users, playlists } from "./soundcloud"
 
 const app = express()
 
@@ -50,7 +51,6 @@ app.post('/mcp', async (req, res) => {
       const res = await tracks({
         q: query.q
       })
-      console.log('res', res[0])
 
       const payload = res
         .map(({ id, title, description, user }) => ({ id, title, description, user }))
@@ -121,4 +121,70 @@ app.delete('/mcp', async (req, res) => {
   }));
 });
 
-export const handler = serverless(app)
+app.post('/api/responses', async (req, res) => {
+  const payload = await fetch(`https://api.openai.com/v1/responses`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`
+    },
+    body: JSON.stringify(req.body)
+  })
+
+  const data = await payload.json()
+
+  res.status(200).json(data)
+})
+
+app.post('/api/audio/speech', async (req, res) => {
+  console.log(req.body)
+  const payload = await fetch(`https://api.openai.com/v1/audio/speech`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`
+    },
+    body: JSON.stringify(req.body)
+  })
+
+  const arrayBuffer = await payload.arrayBuffer()
+  const buffer = Buffer.from(arrayBuffer)
+  res.setHeader('Content-Type', 'audio/mpeg');
+  res.send(buffer);
+})
+
+// app.use((_, res, next) => {
+//   res.set({
+//     // "Cross-Origin-Opener-Policy": "same-origin",
+//     // "Cross-Origin-Embedder-Policy": "require-corp",
+//     "Cross-Origin-Resource-Policy": "cross-origin",
+//     // "Origin-Agent-Cluster": "?1",
+//     "Access-Control-Allow-Origin": "*",
+//     // "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+//     // "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Range"
+//   })
+//   next()
+// })
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/tracks/:id', (req, res) => {
+  const id = req.params.id || '692509783'
+
+  const template = fs.readFileSync(`${__dirname}/public/index.html`)
+
+  if (!template) return res.status(500).send('Error reading template')
+
+  const payload = template.replace(/\{id\}/g, id)
+
+  res.send(payload)
+})
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000')
+})
+
