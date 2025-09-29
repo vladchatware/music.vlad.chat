9da3bc235d08ce63981b776a163950914664f28c
Vlad Rimsha <vlad@jelly.ninja>
2025-09-29 18:26:47 +0700

    Prepare routes

---
+++ app/api/mcp/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse, NextRequest } from 'next/server'
+
+export async function GET(req: NextRequest) {
+  return NextResponse.json([])
+}
+
+export async function POST() {
+  return NextResponse.json([])
+}
+
+export async function DELETE() {
+  return NextResponse.json([])
+}
