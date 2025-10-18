171de5eb0304e8eaa6d0aa1618042cffa03e3305
Vlad Rimsha <vlad@jelly.ninja>
2025-10-18 20:11:36 +0700

    Add SoundCloud auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
 import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server"
+import { ConvexClientProvider } from "./ConvexContextProvider"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ConvexAuthNextjsServerProvider>
       <html lang="en">
         <body style={{ margin: 0 }}>
-          {children}
+          <ConvexClientProvider>
+            {children}
+          </ConvexClientProvider>
         </body>
       </html>
     </ConvexAuthNextjsServerProvider>
--- convex/auth.ts
+++ convex/auth.ts
@@ -1,30 +1,46 @@
 import { convexAuth } from "@convex-dev/auth/server";
+import type { AuthProviderConfig } from "@convex-dev/auth/server";
 import { Anonymous } from "@convex-dev/auth/providers/Anonymous"
 import { MutationCtx } from "./_generated/server";
 
-const Soundcloud = (options) => {
+const Soundcloud: AuthProviderConfig = (options) => {
   return {
     id: 'soundcloud',
     name: 'SoundCloud',
     type: 'oauth',
-    authorization: "https://secure.soundcloud.com/authorize",
-    token: "https://secure.soundcloud.com/oauth/token",
-    userinfo: "https://api.soundcloud.com/me",
-    profile(profile) {
-      console.log(profile)
-      return {
-        id: profile.id,
-        name: profile.username || profile.full_name,
-        email: profile.email,
-        image: profile.avatar_url
+    authorization: 'https://secure.soundcloud.com/authorize',
+    token: 'https://secure.soundcloud.com/oauth/token',
+    userinfo: {
+      url: 'https://api.soundcloud.com/me',
+      async request(context) {
+        const me = await fetch(`https://api.soundcloud.com/me`, {
+          headers: {
+            Authorization: `Bearer ${context.tokens.access_token}`
+          }
+        })
+        return me.json()
       }
     },
-    options
-  }
-}
+    profile(profile) {
+      return {
+        id: String(profile.id),
+        name: profile.username || profile.full_name,
+        email: profile.email,
+        image: profile.avatar_url,
+      };
+    },
+    client: {
+      token_endpoint_auth_method: 'client_secret_post'
+    },
+    options,
+  };
+};
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [Anonymous],
+  providers: [
+    Anonymous,
+    Soundcloud,
+  ],
   callbacks: {
     async afterUserCreatedOrUpdated(ctx: MutationCtx, { userId }) {
       await ctx.db.patch(userId, { trialMessages: 10, trialTokens: 16000000, tokens: 0 })
