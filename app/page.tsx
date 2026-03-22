ca0cd00ef5f914c4a1a10a0f891c351b08e09f59
vladchatware <vladchatware@gmail.com>
2026-03-22 10:07:34 +0700

    fix(overlay): show track card for authenticated users, update default track
    Gate the track card behind non-anonymous user check instead of just
    isAuthenticated. Uncomment the card UI. Update default track ID.

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,5 @@
 import MusicPlayerV2 from '@/components/music-player/MusicPlayerV2'
 
 export default function Page() {
-  return <MusicPlayerV2 initialTrackId={2094321906} />
+  return <MusicPlayerV2 initialTrackId={2048539408} />
 }
--- components/music-player/Overlay.tsx
+++ components/music-player/Overlay.tsx
@@ -57,13 +57,13 @@ export function MusicPlayerOverlay(props: {
       </Container>
 
       <Container 
-        display={isAuthenticated && activeTrack ? "flex" : "none"} 
+        display={isAuthenticated && user && !user.isAnonymous && activeTrack ? "flex" : "none"} 
         marginBottom={20}
         alignItems="center"
         justifyContent="center"
         width="100%"
       >
-        {/* <Card maxWidth={380} width="100%" backgroundColor="rgb(4, 16, 22)">
+        <Card maxWidth={380} width="100%" backgroundColor="rgb(4, 16, 22)">
           <CardContent gap={8} paddingTop={16} paddingX={20} paddingBottom={12}>
             <Image 
               src={activeTrack?.artwork_url} 
@@ -83,7 +83,7 @@ export function MusicPlayerOverlay(props: {
               </Text>
             </CardDescription>
           </CardHeader>
-        </Card> */}
+        </Card>
       </Container>
 
       <Container flexDirection="column" alignItems="center" gap={12} width="100%">
